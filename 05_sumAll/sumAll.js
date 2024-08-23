9de353a512667f6dab26f2670ffba97b6fd5e67d
happysonsad <[kinchun917@gmail.com]>
2024-08-23 21:59:21 +0800

    finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    
+    //return ERROR if -ve numbers
+    if (min < 0 || max < 0) return "ERROR"
 
+
+    //return ERROR if non-integer parameters
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"
+
+    //work w/larger number first
+    /*
+    if (min > max) {
+        const largerOne = min; //create new variables to store the larger numbers
+        min = max; //change the min to max
+        max = largerOne; //assign back the larger number
+    }
+    */
+    
+    if (min > max) [min, max] = [max, min];
+
+    let totalSum = 0;
+    for (let i = min; i <= max; i++) {
+        totalSum += i;
+    } return totalSum;
 };
 
 // Do not edit below this line
