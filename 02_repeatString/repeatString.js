da49ca9596e0326e5705f10983379e3f0f4ba848
happysonsad <[kinchun917@gmail.com]>
2024-08-23 12:18:30 +0800

    finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) return "ERROR"//returns ERROR with negative numbers
+    let string = "";//create a variables to hold the string to return
+    for (let i = 0; i < times; i++) {
+        string = string + word;
+    }//create a loop to repeats the given number of times and 
+    return string;//add the given string to the result on each loop
 };
 
 // Do not edit below this line
