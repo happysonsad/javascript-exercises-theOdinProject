713eefdfae583895cf492b73aedd2f367498a9a8
happysonsad <[kinchun917@gmail.com]>
2024-08-23 12:33:55 +0800

    finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("");//split it into array first 
+    //reverse it
+    //join it back together
 };
 
 // Do not edit below this line
