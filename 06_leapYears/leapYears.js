2707bb69ed4cd54bbe5f0506a8b85b708044e6f5
happysonsad <[kinchun917@gmail.com]>
2024-08-24 14:23:43 +0800

    finished exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function leapYears(year) {
+    if (year % 100 === 0 && year %400 !== 0) {
+        return false;
+    } else if (year % 4 === 0 || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+//npm test leapYears.spec.js
 // Do not edit below this line
 module.exports = leapYears;
