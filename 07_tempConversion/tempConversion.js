7ca7ac8409f3f9fde8b8e60a064d5b9d3ecd6a76
happysonsad <[kinchun917@gmail.com]>
2024-08-24 14:39:00 +0800

    finished 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const convertToCelsius = function() {
+let Celsius = "";
+let Fahrenheit = "";
+
+const convertToCelsius = function convertToCelsius(Fahrenheit) {
+  // Fahrenheit °F to Celsius °C Formula
+  Celsius = (Fahrenheit -32) / (9 / 5);
+  return Math.round(Celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function convertToFahrenheit(Celsius) {
+  // Celsius °C to Fahrenheit °F Formula
+  Fahrenheit = Celsius * (9 / 5) +32;
+  return Math.round(Fahrenheit * 10) / 10;
 };
 
+// npm test tempConversion.spec.js
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
