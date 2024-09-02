c1802a357fe105018d9b4eca238ea605a688e74e
happysonsad <[kinchun917@gmail.com]>
2024-09-02 13:57:01 +0800

    finished 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    const withoutSymbols = "abcdefghijklmnopqrstuvwxyz0123456789";
+    
+    const cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => withoutSymbols.includes(char))
+    .join("");
 
-};
+    const ReversedString = cleanedString.split("").reverse().join("");
+
+    return cleanedString === ReversedString;
+
+}
+// npm test palindromes.spec.js
 
 // Do not edit below this line
 module.exports = palindromes;
