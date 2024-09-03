00d325c58fe46780871d2b6863903dcf22d2bf4c
happysonsad <kinchun917@gmail.com>
2024-09-03 10:18:29 +0800

    finished 12 exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth
+}
 
+
+const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestPerson = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const nextPerson = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestPerson > nextPerson ? oldest : currentPerson;
+    });
 };
-
+// npm test findTheOldest.spec.js
 // Do not edit below this line
 module.exports = findTheOldest;
